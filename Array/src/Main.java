7c3765cd14f8319542b9e305659fba6c3b778121
Wayne <tecman4@gmail.com>
2023-10-31 14:24:03 -0400

    Java testing Array

---
+++ Array/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(50);
+        numbers.insert(60);
+        numbers.removeAt(1);
+        numbers.print();
+        System.out.println(numbers.indexOf(30));
+
+    }
+}

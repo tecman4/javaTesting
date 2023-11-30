babf7bad511d6e599b63a3305b7623f0bf4279ef
Wayne <tecman4@gmail.com>
2023-11-30 16:48:03 -0500

    Working on HashTable with LinkedList

---
+++ HashTableChainingLinkList/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Hashtable;
+
+public class Main {
+    public static void main(String[] args) {
+        new KeyValuePair();
+        KeyValuePair.put(5,"Wayne");
+        var whoami = KeyValuePair.get(5);
+    }
+}

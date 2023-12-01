c5ac830430df3505e5b7d4ae2d2aac6dac21a85b
Wayne <tecman4@gmail.com>
2023-12-01 16:46:06 -0500

    Working on Hash Table

--- HashTableChainingLinkList/src/Main.java
+++ HashTableChainingLinkList/src/Main.java
@@ -3,8 +3,27 @@ import java.util.Hashtable;
 
 public class Main {
     public static void main(String[] args) {
-        new KeyValuePair();
-        KeyValuePair.put(5,"Wayne");
-        var whoami = KeyValuePair.get(5);
+        HashTable table = new HashTable();
+        table.put(6,"A");// index 1
+        table.put(8,"B");// index 3
+        table.put(11,"C");// index 1
+        table.put(6,"A+");// index 1
+        table.remove(6);
+        System.out.println(table.get(6));
+
+
+
+
+
+
+
+
+//        KeyValuePair pair = new KeyValuePair();
+//       pair.put(5, "Wayne");
+//        pair.put(6, "Matthew");
+//        String whoamiw = pair.get(5);
+//        System.out.println(whoamiw);
+//        String whoamim = pair.get(6);
+//        System.out.println(whoamim);
     }
 }

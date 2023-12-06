806fc279e606d8acae4913844b5b8639ab7a8ade
Wayne <tecman4@gmail.com>
2023-12-06 16:46:02 -0500

    Working on Tree Travseral, Completed Pre, Post and in order

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         var tree = new Tree();
-        tree.insert(5);
-        tree.insert(6);
-        tree.insert(4);
-        tree.insert(3);
         tree.insert(7);
-        System.out.println(tree);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        tree.find(8);
+        System.out.println(tree.find(9));
+        tree.traversePreOrder();
+        System.out.println(tree.find(9));
+        tree.traverseInOrder();
+        System.out.println(tree.find(9));
+        tree.traversePostOrder();
     }
 }
--- Tree/src/Tree.java
+++ Tree/src/Tree.java
@@ -1,57 +1,88 @@
 public class Tree {
-
     private class Node{
         private int value;
         private Node leftChild;
         private Node rightChild;
-
         public Node(int value) {
             this.value = value;
         }
-
+        @Override
+        public String toString(){
+            return "Node="+ value;
+        }
     }
+
     private Node root;
 public boolean find(int value){
-        if(root.value == value)
+    var current = root;
+    while(current!= null) {
+        if (current.value == value)
             return true;
-        var current = root;
-        if(value > current.value){
-
+        else if (value > current.value) {
+            current = current.rightChild;
         }
-        return false;
+        else  {
+            current = current.leftChild;
+        }
+    }
+    return false;
 }
 public void insert(int value){
     var current = root;
+    var node = new Node(value);
         if(root == null){
             root = new Node(value);
              return;
         }
-
         while(true) {
-
             if (value > current.value) {
-
                 if(current.rightChild == null) {
-                    current.rightChild = new Node(value);
+                    current.rightChild = node;
                     current = current.rightChild;
                     current.value = value;
                     break;
                 }
                 current = current.rightChild;
             }
-            if (value < current.value) {
-
+            else {
                 if(current.leftChild == null) {
-                    current.leftChild = new Node(value);
+                    current.leftChild =  node;
                     current = current.leftChild;
                     current.value = value;
                     break;
                 }
                 current = current.leftChild;
             }
-
         }
-
-
 }
+public void traversePreOrder(){
+    traversePreOrder(root);
+}
+private void traversePreOrder(Node root){
+    if(root ==null)
+        return;
+    System.out.println(root.value);
+    traversePreOrder(root.leftChild);
+    traversePreOrder(root.rightChild);
+}
+public void traverseInOrder(){
+traverseInOrder(root);
+}
+private void traverseInOrder(Node root) {
+    if (root == null)
+        return;
+    traverseInOrder(root.leftChild);
+    System.out.println(root.value);
+    traverseInOrder(root.rightChild);
+    }
+    public void traversePostOrder(){
+        traversePostOrder(root);
+    }
+    private void traversePostOrder(Node root){
+        if(root ==null)
+            return;
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
 }

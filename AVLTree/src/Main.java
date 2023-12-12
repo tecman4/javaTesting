cde61560364593f8d97fd4ea52785c3c3701d816
Wayne <tecman4@gmail.com>
2023-12-12 16:43:55 -0500

    Working on AVLTree

---
+++ AVLTree/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+
+
+    }
+}

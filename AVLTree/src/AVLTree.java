a9eef5d7064693d20d97378dead3e6764ddbd5ba
Wayne <tecman4@gmail.com>
2023-12-13 18:51:59 -0500

    Working on AVL Node tree

--- AVLTree/src/AVLTree.java
+++ AVLTree/src/AVLTree.java
@@ -3,6 +3,7 @@ public class AVLTree {
         private int value;
         private AVLNode leftChild;
         private AVLNode rightChild;
+        private int height;
 
         public AVLNode(int value) {
             this.value = value;
@@ -13,6 +14,7 @@ public class AVLTree {
             return "Value=" + value;
         }
     }
+
     private AVLNode root;
     public void insert(int value){
     root= insert(root, value);
@@ -24,6 +26,28 @@ public class AVLTree {
             root.leftChild = insert(root.leftChild, value);
         else
             root.rightChild = insert(root.rightChild, value);
+
+        root.height = Math.max(height(root.leftChild), height(root.rightChild)) +1;
+
+        if(isLeftHeavy(root))
+        System.out.println(root.value + " is left heavy");
+    else if(balanceFactor(root) < -1)
+        System.out.println(root.value + " is right heavy");
+
+
     return root;
+    }
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+    private boolean isRightHeavy(AVLNode node) {
+        return balanceFactor(node)  < -1;
+    }
+
+    private int balanceFactor(AVLNode node){
+        return height(root.leftChild)- height(root.rightChild);
+    }
+    private int height(AVLNode node){
+        return(node == null) ? -1 : node.height;
     }
 }

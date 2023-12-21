a74a93b79d0bc6179e03b8e1a86aac8de6fda9e6
Wayne <tecman4@gmail.com>
2023-12-21 16:40:42 -0500

    Working on Heapify

--- Heapify/src/Main.java
+++ Heapify/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] numbers = {5,3,8,4,1,2};
+        MaxHeap.heapify(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }

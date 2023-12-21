2cb06ed0bb4eeaee303924e4c4c92101e311b56f
Wayne <tecman4@gmail.com>
2023-12-21 14:58:00 -0500

    Starting Heapify

--- Heap/src/Heap.java
+++ Heap/src/Heap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Heap {
     private int[] items;
     private int size;
@@ -31,9 +33,61 @@ public class Heap {
         var temp = items[first];
         items[first] = items[second];
         items[second] = temp;
+    }
+    private int leftChild(int index){
+        return items[leftChildIndex(index)];
+    }
+    private int rightChild(int index){
+        return items[rightChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index){
+        return index * 2 + 1;
+    }
+    private int rightChildIndex(int index){
+        return index * 2 + 1;
+    }
+    private boolean isValidParent(int index){
+        if(!hasLeftChild(index))
+            return true;
+        if(!hasRightChild(index))
+            return items[index] >= leftChild(index);
+       return items[index] >= leftChild(index) && items[index] >= rightChild(index);
+    }
+    private int largerChildIndex(int index){
+        if(!hasLeftChild(index))
+            return index;
+        if(!hasRightChild(index))
+            return leftChildIndex(index);
+        return (leftChild(index) > rightChild(index)) ? leftChildIndex(index) : rightChildIndex(index);
 
     }
-    public void remove() {
+    private boolean hasLeftChild(int index){
+        return leftChildIndex(index) <= size;
+    }
+    private boolean hasRightChild(int index){
+        return rightChildIndex(index) <= size;
+    }
+    public boolean isEmpty(){
+        return size ==0;
+    }
+    private void bubbleDown(){
+        var index =0;
+        while(index <= size && !isValidParent(index)){
+            var largerChildIndex = largerChildIndex(index);
+            swap(index,largerChildIndex);
+            index = largerChildIndex(index);
+        }
+    }
+    public int remove() {
+        if(isEmpty())
+            throw new IllegalStateException();
+        var root = items[0];
+        items[0] = items[--size];
+        bubbleDown();
+        return root;
+
 
     }
+
 }
--- Heap/src/Main.java
+++ Heap/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args){
 var heap = new Heap(10);
     heap.insert(10);
+    heap.insert(20);
+    heap.insert(50);
+    heap.insert(40);
+    heap.insert(70);
     heap.remove();
+    System.out.println(heap.remove());
     }
 }

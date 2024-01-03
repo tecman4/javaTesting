9cc5c7cab8eb495f439af4471d46be0ac395835d
Wayne <tecman4@gmail.com>
2024-01-03 16:50:24 -0500

    Working on Trie Contains Boolean

--- Trie/src/Main.java
+++ Trie/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         var trie = new Trie();
-        trie.insert();
+        trie.insert("cat");
+        trie.insert("can");
+        trie.contains("can");
+        System.out.println("Done");
     }
 }
--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -1,32 +1,62 @@
-public class Trie {
-    public void insert() {
-    }
+import java.util.HashMap;
 
-    private class Node{
-        public static int ALPHABET_SIZE = 26;
+public class Trie {
+    public static int ALPHABET_SIZE = 26;
+
+
+
+
+    private class Node {
         private char value;
-        private Node [] children = new Node[ALPHABET_SIZE ];
-        private  boolean isEndOfWord;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        public char getValue() {
+            return value;
+        }
 
         @Override
         public String toString() {
             return "value=" + value;
         }
+        public boolean hasChild(char ch){
+            return children.containsKey(ch);
+        }
+        public void addChild(char ch){
+            children.put(ch, new Node(ch));
+
+        }
+        public Node getChild(char ch){
+            return children.get(ch);
+        }
+
+    }
         private Node root = new Node(' ');
-        public Node(char value) {
-            this.value = value;
-        }
-    public void insert( String word){
+
+        public void insert(String word) {
             var current = root;
-            for(var ch : word.toCharArray()){
-                var index = ch - 'a';
-                if(current.children[index] ==null){
-                    current.children[index] = new Node(ch);
-                    current = current.children[index];
+            for (var ch : word.toCharArray()) {
+                if (!current.hasChild(ch))
+                    current.addChild(ch);
+                current = current.getChild(ch);
                 }
-                current.isEndOfWord = true;
-            }
+            current.isEndOfWord = true;
         }
+    public boolean contains(String word) {
+        var current = root;
+        for(var ch :word.toCharArray()){
+            if(!current.hasChild(ch))
+                return false;
+        
+        }
+
+    }
     }
 
-}
+
+
+

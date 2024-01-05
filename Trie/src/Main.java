d5de1e05777f32f7067c434e6eba4fbcbd7b2f70
Wayne <tecman4@gmail.com>
2024-01-05 16:13:04 -0500

    Completed Trie Remove

--- Trie/src/Main.java
+++ Trie/src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         var trie = new Trie();
         trie.insert("cat");
-        trie.insert("can");
+        trie.insert("catton");
         trie.contains("can");
-        System.out.println(trie.contains("can"));
-        trie.traverse();
+        trie.remove("catton");
+        System.out.println(trie.contains("catton"));
+        System.out.println(trie.contains("cat"));
+        //        trie.traverse();
     }
 }
--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -4,8 +4,6 @@ public class Trie {
     public static int ALPHABET_SIZE = 26;
 
 
-
-
     private class Node {
         private char value;
         private HashMap<Character, Node> children = new HashMap<>();
@@ -23,57 +21,89 @@ public class Trie {
         public String toString() {
             return "value=" + value;
         }
-        public boolean hasChild(char ch){
+
+        public boolean hasChild(char ch) {
             return children.containsKey(ch);
         }
-        public void addChild(char ch){
+
+        public void addChild(char ch) {
             children.put(ch, new Node(ch));
 
         }
-        public Node getChild(char ch){
+
+        public Node getChild(char ch) {
             return children.get(ch);
         }
-        public Node[] getChildren(){
+
+        public Node[] getChildren() {
             return children.values().toArray(new Node[0]);
+        }
+        public boolean hasChildren(){
+            return !children.isEmpty();
+        }
+        public void removeChild(char ch){
+            children.remove(ch);
         }
 
     }
-        private Node root = new Node(' ');
 
-        public void insert(String word) {
-            var current = root;
-            for (var ch : word.toCharArray()) {
-                if (!current.hasChild(ch))
-                    current.addChild(ch);
-                current = current.getChild(ch);
-                }
-            current.isEndOfWord = true;
-        }
-    public boolean contains(String word) {
-            if(word == null)
-                return false;
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
         var current = root;
-        for(var ch :word.toCharArray()){
-            if(!current.hasChild(ch))
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
+        }
+        current.isEndOfWord = true;
+    }
+
+    public boolean contains(String word) {
+        if (word == null)
+            return false;
+        var current = root;
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch))
                 return false;
             current = current.getChild(ch);
         }
-            return current.isEndOfWord;
+        return current.isEndOfWord;
 
     }
-    public void traverse(){
+
+    public void traverse() {
         traverse(root);
     }
-    private void traverse(Node root){
+
+    private void traverse(Node root) {
         System.out.println(root.value);
-        for(var child :root.getChildren())
+        for (var child : root.getChildren())
             traverse(child);
     }
-    private boolean delete(String word){
+
+    public void remove(String word) {
+        if(word ==null)
+            return;
+        remove(root, word, 0);
+    }
+
+    private void remove(Node root, String word,int index) {
+        if(index == word.length()){
+            root.isEndOfWord =false;
+            return;
+        }
+        var ch = word.charAt(index);
+        var child = root.getChild(ch);
+        if(child == null)
+            return;
+        remove(child, word, index+1);
+
+        if(!child.hasChildren() && !child.isEndOfWord)
+            root.removeChild(ch);
 
     }
-    }
-
+}
 
 
 

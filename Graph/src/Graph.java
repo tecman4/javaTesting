cc297c9443616c9448431055b716116cc1e9901f
Wayne <tecman4@gmail.com>
2024-01-29 16:47:50 -0500

    Started Building a Graph

---
+++ Graph/src/Graph.java
@@ -0,0 +1,21 @@
+
+public class Graph {
+    private String label;
+    private void node(String label){
+        this.str = label;
+
+    }
+    public void addNode()
+        new node 
+
+
+    removeNode(label)
+
+    addEdge(from, to)
+
+    removeEdge(from, to)
+    print()
+            ex: A is connected to [B,C]
+            B is connected with A
+
+}

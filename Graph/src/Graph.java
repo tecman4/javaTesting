95f46e1cf6b251e5006306d1092299e510812541
Wayne <tecman4@gmail.com>
2024-01-31 16:45:33 -0500

    Working on Graph

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,21 +1,37 @@
+import java.util.HashMap;
 
 public class Graph {
     private String label;
-    private void node(String label){
-        this.str = label;
+    private HashMap<String, Node> children = new HashMap<>();
+
+    public void addNode(String label){
+        children.put(label, new Node(label));
+    }
+    public void addEdge(String from, String to){
+        children.put(from, new Node(to));
 
     }
-    public void addNode()
-        new node 
 
+    public void print() {
+        System.out.println(toString(children.get("A")));
+    }       public String toString() {
+        return "" + label;
+    }
+    private class Node {
+        private String label;
 
-    removeNode(label)
+        public Node(String label) {
+            this.label = label;
+        }
+    }
 
-    addEdge(from, to)
-
-    removeEdge(from, to)
-    print()
-            ex: A is connected to [B,C]
-            B is connected with A
+//    removeNode(label)
+//
+//    addEdge(from, to)
+//
+//    removeEdge(from, to)
+//    print()
+//            ex: A is connected to [B,C]
+//            B is connected with A
 
 }
--- Graph/src/Main.java
+++ Graph/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addEdge("A","B");
+        graph.print();
     }
 }

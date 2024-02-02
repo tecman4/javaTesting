d8b1b93faf3a33a293ab4f23a028239b6637f250
Wayne <tecman4@gmail.com>
2024-02-02 16:58:14 -0500

    Working on Graph

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -2,21 +2,36 @@ import java.util.HashMap;
 
 public class Graph {
     private String label;
-    private HashMap<String, Node> children = new HashMap<>();
+    private HashMap<String, Node> edges = new HashMap<>();
 
     public void addNode(String label){
-        children.put(label, new Node(label));
+
+        edges.put(label, new Node(null));
     }
     public void addEdge(String from, String to){
-        children.put(from, new Node(to));
+        edges.put(from, new Node(to));
 
     }
 
     public void print() {
-        System.out.println(toString(children.get("A")));
-    }       public String toString() {
-        return "" + label;
+        for (Object objectName : edges.keySet()) {
+            if(edges.get(objectName) != null)
+                System.out.println(objectName + " is connected to " + edges.get(objectName).toString() );
+
+        }
     }
+    @Override
+    public String toString() {
+        return "Node{" +
+                "nodeName='" + edges + '\'' +
+                '}';
+    }
+
+    public void removeEdge(String remove) {
+        edges.put(remove, null);
+
+    }
+
     private class Node {
         private String label;
 
--- Graph/src/Main.java
+++ Graph/src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         var graph = new Graph();
         graph.addNode("A");
+        graph.addNode("C");
         graph.addEdge("A","B");
+        graph.addNode("B");
+        graph.addEdge("B","A");
+        graph.addEdge("B","D");
+        graph.removeEdge("B");
         graph.print();
     }
 }
